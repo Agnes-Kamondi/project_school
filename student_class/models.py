d21ee6e4b629603e4c69f0ae8102de44c04fb683
Agnes-Kamondi <kamondiagnesauma@gmail.com>
2024-07-15 15:36:42 +0300

    added a relationship between class and class period

--- student_class/models.py
+++ student_class/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from classperiod.models import Class_Period
 from teacher.models import Teacher
 
 
@@ -14,6 +15,7 @@ class Student_Class(models.Model):
     capacity = models.IntegerField()
     room_number = models.IntegerField()
     specialty = models.TextField(blank=True, null=True)
+    class_period = models.ForeignKey(Class_Period, on_delete=models.SET_NULL,null = True, related_name = 'student_class')
     
     def __str__(self):
         return self.name
